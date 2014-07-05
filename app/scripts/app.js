5690db602d0916e322b6ce8082524cbfbb8cea37
rem{e}koh <git@remekoh.me>
2014-07-05 12:29:21 -0500

    Converted collection view to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,24 @@
  //require('./album');
  //require('./collection');
  //require('./profile');
+
+// Example album.
+var albumPicasso = {
+  name: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: '/images/album-placeholder.png',
+
+  songs: [
+      { name: 'Blue', length: '4:26' },
+      { name: 'Green', length: '3:14' },
+      { name: 'Red', length: '5:01' },
+      { name: 'Pink', length: '3:21'},
+      { name: 'Magenta', length: '2:15'}
+    ]
+};
+ 
  
  blocJams = angular.module('BlocJams', ['ui.router']);
  blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
@@ -12,6 +30,14 @@
      controller: 'Landing.controller',
      templateUrl: '/templates/landing.html'
    });
+
+
+   $stateProvider.state('collection', {
+     url: '/collection',
+     controller: 'Collection.controller',
+     templateUrl: '/templates/collection.html'
+   });
+
  }]);
  
  // This is a cleaner way to call the controller than crowding it on the module definition.
@@ -33,4 +59,11 @@
      '/images/album-placeholders/album-8.jpg',
      '/images/album-placeholders/album-9.jpg',
    ];
+}]);
+
+blocJams.controller('Collection.controller', ['$scope', function($scope) {
+ $scope.albums = [];
+  for (var i = 0; i < 33; i++) {
+    $scope.albums.push(angular.copy(albumPicasso));
+ }
 }]);
